35cecdf2b9d6c50775ab2b738b102b7d9c007cfa
Alejandro <Alejandro@192.168.1.32>
2016-04-11 00:15:17 +0200

    Primer Commit

---
+++ src/PaqueteAlfaro/ClaseAlfaro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteAlfaro;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class ClaseAlfaro {
+    
+    public ClaseAlfaro()
+    {
+      
+    }
+    
+    public void mostrarSaludo()
+    {
+        System.out.println("Hola, bienvenido al pozo");
+    }
+}
