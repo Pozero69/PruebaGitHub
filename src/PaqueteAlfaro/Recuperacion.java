1e9ab6515fa48a66d25fd82a8959140311d1b497
Alejandro <Alejandro@192.168.1.32>
2016-04-11 00:55:21 +0200

    Primer Commit Segundo Commit Tercer Commit

---
+++ src/PaqueteAlfaro/Recuperacion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteAlfaro;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Recuperacion {
+    
+    public Recuperacion()
+    {
+        
+    }
+    
+    public void pruebaSaludo()
+    {
+        System.out.println("Saludando al mundo");
+    }
+    
+}
