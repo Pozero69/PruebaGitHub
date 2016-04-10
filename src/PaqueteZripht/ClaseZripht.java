11792f295d6b1ece85dcb99ba95881a96397fcee
zriph <zriph@ZriphtPC>
2016-04-11 00:41:24 +0200

    Commit Zripht

---
+++ src/PaqueteZripht/ClaseZripht.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteZripht;
+
+/**
+ *
+ * @author zriph
+ */
+public class ClaseZripht {
+    public ClaseZripht()  {
+        
+    }
+    
+    public void mostrarSaludo() {
+        
+        System.out.println("Hola a todos :D");
+    }
+    public void mostrarDespedida() {
+        System.out.println("Adios a todos :_(");
+    }
+}
